af27803c9481fedd7100ea21ed5084c14615f08d
dennis zhuang <killme2008@gmail.com>
2014-10-15 23:51:03 +0800

    Added fun and letfun macro

--- src/defun.clj
+++ src/defun.clj
@@ -1,14 +1,60 @@
 (ns
-  ^{:author "dennis <killme2008@gmail.com>"
-    :doc "A beautiful macro to define clojure functions"}
+    ^{:author "dennis <killme2008@gmail.com>"
+      :doc "A beautiful macro to define clojure functions"}
   defun
   (:require [clojure.core.match :refer [match]]
             [clojure.tools.macro :refer [name-with-attributes]]
             [clojure.walk :refer [postwalk]]))
 
+(defmacro fun
+  "Defines a function just like clojure.core/fn with parameter pattern matching.
+   See
+     https://github.com/killme2008/defun
+  for details."
+  [& sigs]
+  {:forms '[(fun name? [params* ] exprs*) (fun name? ([params* ] exprs*)+)]}
+  (let [name (when(symbol? (first sigs)) (first sigs))
+        sigs (if name (next sigs) sigs)
+        sigs (if (vector? (first sigs))
+               (list sigs)
+               (if (seq? (first sigs))
+                 sigs
+                 ;; Assume single arity syntax
+                 (throw (IllegalArgumentException.
+                         (if (seq sigs)
+                           (str "Parameter declaration "
+                                (first sigs)
+                                " should be a vector")
+                           (str "Parameter declaration missing"))))))
+        sigs (postwalk
+              (fn [form]
+                (if (and (list? form) (= 'recur (first form)))
+                  (list 'recur (cons 'vector (next form)))
+                  form))
+              sigs)
+        sigs `([& args#]
+                 (match (vec args#)
+                        ~@(mapcat
+                           (fn [[m & more]]
+                             [m (cons 'do more)])
+                           sigs)))]
+    (list* 'fn (if name
+                 (cons name sigs)
+                 sigs))))
+
+(defmacro letfun
+  "letfn with parameter pattern matching."
+  {:forms '[(letfun [fnspecs*] exprs*)]}
+  [fnspecs & body]
+  `(letfn* ~(vec (interleave (map first fnspecs)
+                             (map #(cons `fun %) fnspecs)))
+           ~@body))
+
 (defmacro defun
- "Define a function just like defn, but using core.match to match parameters.
-  See https://github.com/killme2008/defun for details."
+  "Define a function just like clojure.core/defn, but using core.match to
+  match parameters.See
+    https://github.com/killme2008/defun
+  for details."
   [name & fdecl]
   (let [[name body] (name-with-attributes name fdecl)
         body (if (vector? (first body))
@@ -18,11 +64,11 @@
               meta
               (assoc :arglists (list 'quote (@#'clojure.core/sigs body))))
         body (postwalk
-               (fn [form]
-                 (if (and (list? form) (= 'recur (first form)))
-                   (list 'recur (cons 'vector (next form)))
-                   form))
-               body)]
+              (fn [form]
+                (if (and (list? form) (= 'recur (first form)))
+                  (list 'recur (cons 'vector (next form)))
+                  form))
+              body)]
     `(defn ~name ~m [& args#]
        (match (vec args#)
               ~@(mapcat
--- test/defun/core_test.clj
+++ test/defun/core_test.clj
@@ -58,27 +58,27 @@
 (deftest test-match-literals
   (testing "test1"
     (defun test1
-    ([true false] 1)
-    ([true true] 2)
-    ([false true] 3)
-    ([false false] 4))
+      ([true false] 1)
+      ([true true] 2)
+      ([false true] 3)
+      ([false false] 4))
     (is (= 2 (test1 true true)))
     (is (= 4 (test1 false false)))))
 
 (deftest test-match-vector
   (testing "test3"
     (defun test3
-    ([[_ _ 2]] :a0)
-    ([[1 1 3]] :a1)
-    ([[1 2 3]] :a2))
+      ([[_ _ 2]] :a0)
+      ([[1 1 3]] :a1)
+      ([[1 2 3]] :a2))
     (is (= :a2 (test3 [1 2 3])))
     (is (= :a0 (test3 [3 3 2])))
     (is (= :a1 (test3 [1 1 3]))))
   (testing "test2"
     (defun test2
-    ([([1] :seq)] :a0)
-    ([([1 2] :seq)] :a1)
-    ([([1 2 nil nil nil] :seq)] :a2))
+      ([([1] :seq)] :a0)
+      ([([1 2] :seq)] :a1)
+      ([([1 2 nil nil nil] :seq)] :a2))
     (is (= :a2 (test2 [1 2 nil nil nil])))))
 
 (deftest test-private-macro
@@ -92,7 +92,7 @@
     (defun test1
       ([1] 1)
       ([x] (println "Square")
-           (* 2 x)))
+         (* 2 x)))
     (with-out-str
       (is (= 4 (test1 2))))
     (is (= "Square") (with-out-str (test1 2)))))
@@ -105,3 +105,29 @@
       ([a b] "unknown."))
     (is (= "hello world" (-> #'hello meta :doc)))
     (is (= '([name] [a b])) (-> #'hello meta :arglists))))
+
+(deftest test-fun
+  (testing "fun"
+    (is (= 0 ((fun [] 0))))
+    (is (= 0 ((fun test [] 0))))
+    (is (= 0 ((fun ([] 0) ([a] a) ([a b] b)))))
+    (is (= 2 ((fun ([] 0) ([a] a) ([a b] b)) 2)))
+    (is (= 1 ((fun ([] 0) ([a] a) ([a b] b)) 1)))
+    (is (= 2 ((fun ([] 0) ([a] a) ([a b] b)) 1 2)))))
+
+(deftest test-letfun
+  (testing "letfun"
+    (letfun [(twice [x]
+                    (* x 2))
+             (six-times [y]
+                        (* (twice y) 3))
+             (accum
+              ([0 ret] ret)
+              ([n ret] (recur (dec n) (+ n ret)))
+              ([n] (recur n 0)))]
+            (is (= 30 (twice 15)))
+            (is (= 90) (six-times 15))
+            (is (= 5050 (accum 100))))
+    (is (nil? (resolve 'six-times)))
+    (is (nil? (resolve 'twice)))
+    (is (nil? (resolve 'accum)))))
